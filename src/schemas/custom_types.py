0bcd29ed7552b8368d2a4100a12313ac9cc75e67
shab <d.shabin@gmail.com>
2021-12-06 15:29:11 +0400

    Add generci strict

--- src/schemas/custom_types.py
+++ src/schemas/custom_types.py
@@ -72,20 +72,26 @@ class StrictDict(metaclass=CustomFieldMeta):
 
         for key in v:
             try:
-                key_type(key)
-            except ValidationError as e:
-                errors.append("")
+                key_type.validate(key)
+            except TypeError as e:
+                errors.append(ErrorWrapper(exc=e, loc=""))
 
             try:
-                value_type(v[key])
-            except ValidationError as e:
-                errors.append("")
+                value_type.validate((v[key]))
+            except TypeError as e:
+                errors.append(ErrorWrapper(exc=e, loc=""))
+
+        if errors:
+            raise ValidationError(
+                            model=cls,
+                            errors=errors
+                        )
         return v
 
 
 class Foo(BaseModel):
-    c: StrictDict[StrictInt, StrictInt]
-    d: StrictDict[StrictStr, StrictInt]
+    c: StrictDict[Strict[int], Strict[int]]
+    d: StrictDict[Strict[str], Strict[str]]
 
 
 Foo.validate({
@@ -93,83 +99,84 @@ Foo.validate({
     "d": {"1": 1}
 })
 
-
-class StrictType(BaseModel):
-    class Config:
-        key_field = "type"
-
-
-class Polygon(StrictType):
-    type: Literal['polygon']
-    other: StrictInt
-
-
-class NoPolygon(StrictType):
-    type: Literal['nopolygon']
-    other: StrictStr
-
-
-class Golygon(StrictType):
-    type: Literal['golygon']
-    other1: StrictStr
-
-
-class Lolygon(StrictType):
-    type: Literal['lolygon']
-    other1: StrictStr
-
-
-
-class PolyMorphList(metaclass=CustomFieldMeta):
-    key_value_type: Any = None
-
-    @classmethod
-    def __get_validators__(cls):
-        yield cls.validate
-
-    @classmethod
-    def validate(cls, values):
-        errors = []
-        for loc, instance in enumerate(values):
-            for model in cls.key_value_type:
-
-                # check if the model is subclass of StrictType
-                if not issubclass(model, StrictType):
-                    raise ValidationError(model=model,errors=[
-                        ErrorWrapper(exc=MissingError(), loc=(loc,model.__name__))
-                    ])
-
-                # check if identifier field exist
-                if not model.__fields__.get(model.Config.key_field):
-                    raise ValidationError(model=model,errors=[
-                        ErrorWrapper(exc=MissingError(), loc=(loc,model.__name__,model.Config.key_field))
-                    ])
-
-                # check if identifier field is literal
-                if "Literal" not in str(model.__fields__.get(model.Config.key_field).type_):
-                    raise ValidationError(model=model, errors=[
-                        ErrorWrapper(exc=TypeError("Literal expected."), loc=(loc, model.__name__, model.Config.key_field))
-                    ])
-
-                # Validate literal field only
-                try:
-                    model.__fields__.get(model.Config.key_field).validators[0](None, instance[model.Config.key_field], None, None, None)
-                except WrongConstantError:
-                    continue
-
-                # Validate instance
-                try:
-                    model.validate(instance)
-                    break
-                except ValidationError as e:
-                    errors.append(ErrorWrapper(exc=e.raw_errors[0].exc,loc= tuple([loc] + list(e.errors()[0]['loc']))))
-
-        if errors:
-            raise ValidationError(
-                model=cls,
-                errors=errors
-            )
-        return values
+#
+#
+# class StrictType(BaseModel):
+#     class Config:
+#         key_field = "type"
+#
+#
+# class Polygon(StrictType):
+#     type: Literal['polygon']
+#     other: StrictInt
+#
+#
+# class NoPolygon(StrictType):
+#     type: Literal['nopolygon']
+#     other: StrictStr
+#
+#
+# class Golygon(StrictType):
+#     type: Literal['golygon']
+#     other1: StrictStr
+#
+#
+# class Lolygon(StrictType):
+#     type: Literal['lolygon']
+#     other1: StrictStr
+#
+#
+#
+# class PolyMorphList(metaclass=CustomFieldMeta):
+#     key_value_type: Any = None
+#
+#     @classmethod
+#     def __get_validators__(cls):
+#         yield cls.validate
+#
+#     @classmethod
+#     def validate(cls, values):
+#         errors = []
+#         for loc, instance in enumerate(values):
+#             for model in cls.key_value_type:
+#
+#                 # check if the model is subclass of StrictType
+#                 if not issubclass(model, StrictType):
+#                     raise ValidationError(model=model,errors=[
+#                         ErrorWrapper(exc=MissingError(), loc=(loc,model.__name__))
+#                     ])
+#
+#                 # check if identifier field exist
+#                 if not model.__fields__.get(model.Config.key_field):
+#                     raise ValidationError(model=model,errors=[
+#                         ErrorWrapper(exc=MissingError(), loc=(loc,model.__name__,model.Config.key_field))
+#                     ])
+#
+#                 # check if identifier field is literal
+#                 if "Literal" not in str(model.__fields__.get(model.Config.key_field).type_):
+#                     raise ValidationError(model=model, errors=[
+#                         ErrorWrapper(exc=TypeError("Literal expected."), loc=(loc, model.__name__, model.Config.key_field))
+#                     ])
+#
+#                 # Validate literal field only
+#                 try:
+#                     model.__fields__.get(model.Config.key_field).validators[0](None, instance[model.Config.key_field], None, None, None)
+#                 except WrongConstantError:
+#                     continue
+#
+#                 # Validate instance
+#                 try:
+#                     model.validate(instance)
+#                     break
+#                 except ValidationError as e:
+#                     errors.append(ErrorWrapper(exc=e.raw_errors[0].exc,loc= tuple([loc] + list(e.errors()[0]['loc']))))
+#
+#         if errors:
+#             raise ValidationError(
+#                 model=cls,
+#                 errors=errors
+#             )
+#         return values
 
 # class Jungle(BaseModel):
 #     tree: PolyMorphList[Polygon, NoPolygon, Golygon, Lolygon]
