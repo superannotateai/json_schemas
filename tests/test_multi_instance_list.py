592bb5df2cbb309ac5bcefb99cf5e0ecc100d668
shab <d.shabin@gmail.com>
2021-12-08 15:07:29 +0400

    Add test

---
+++ tests/test_multi_instance_list.py
@@ -0,0 +1,61 @@
+from unittest import TestCase
+from pydantic import BaseModel
+from pydantic import Field
+from src.schemas.custom_types import BaseDiscriminatedInstances
+from typing import List
+from typing import Literal
+from typing import Optional
+from typing import Union
+from pydantic import StrictStr
+from pydantic import StrictInt
+from pydantic.error_wrappers import ValidationError
+
+class TestMultiInstance(TestCase):
+
+    def test_multi_list(self):
+
+        class Polygon(BaseModel):
+            type: Literal["polygon"]
+            string_field: StrictStr
+
+        class Lolygon(BaseModel):
+            type: Literal["lolygon"]
+            integer_field: StrictInt
+
+        class TestMultipleInstances(BaseDiscriminatedInstances):
+            __root__: Union[Polygon, Lolygon]
+
+        class TestBaseModel(BaseModel):
+            data: Optional[List[TestMultipleInstances]] = Field(list())
+
+        TestBaseModel(**{
+            "data": [
+                {
+                    "type": "polygon",
+                    "string_field": "3"
+                },
+                {
+                    "type": "lolygon",
+                    "integer_field": 3
+                }
+            ]
+        })
+
+        msg = ''
+        try:
+            TestBaseModel(**{
+                "data": [
+                    {
+                        "type": "polygon",
+                        "string_field": "3"
+                    },
+                    {
+                        "type": "lolygon",
+                        "integer_field": "3"
+                    }
+                ]
+            })
+        except ValidationError as e:
+            msg = str(e)
+        self.assertEqual(msg, "1 validation error for TestBaseModel\ndata -> 1 -> integer_field\n  value is not a valid integer (type=type_error.integer)")
+
