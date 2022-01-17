9c74ebf3794a6a6a6cde4a43bbf0208743620444
Shabin Dilanchian <d.shabin@gmail.com>
2022-01-17 14:23:36 +0400

    Add get version

--- setup.py
+++ setup.py
@@ -1,15 +1,23 @@
+import os
+import re
 from pathlib import Path
 from setuptools import find_packages, setup
 
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
+def get_version():
+    init = open(os.path.join(this_directory, 'src', 'superannotate_schemas', '__init__.py')).read()
+    match = re.search(r'^__version__ = [\'"]([^\'"]+)[\'"]', init, re.M)
+    if not match:
+        raise RuntimeError('Unable to find version string.')
+    return match.group(1)
 
 setup(
     name='superannotate_schemas',
     long_description=long_description,
     long_description_content_type='text/markdown',
-    version='1.0.2b3',
+    version=get_version(),
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     description='SuperAnnotate JSON Schemas',
