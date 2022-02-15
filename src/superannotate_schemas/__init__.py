f0b2a010657917225a0dd1c718802f18b09846d6
Shabin Dilanchian <d.shabin@gmail.com>
2022-02-15 13:28:00 +0400

    Update __init__.py

--- src/superannotate_schemas/__init__.py
+++ src/superannotate_schemas/__init__.py
@@ -7,7 +7,7 @@ sys.path.append(WORKING_DIR)
 from superannotate_schemas.validators import AnnotationValidators
 
 
-__version__ = '1.0.24b'
+__version__ = '1.0.39b1'
 
 __all__ = [
     "__version__",
