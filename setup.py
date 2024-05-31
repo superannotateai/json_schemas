125e72ae9107ab410f8956e2215b273f5b9b9922
nareksa <narekm@superannotate.com>
2024-05-31 15:21:48 +0400

    update setup install_requires conf

--- setup.py
+++ setup.py
@@ -6,12 +6,19 @@ from setuptools import find_packages, setup
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
+
 def get_version():
     init = open(os.path.join(this_directory, 'src', 'superannotate_schemas', '__init__.py')).read()
     match = re.search(r'^__version__ = [\'"]([^\'"]+)[\'"]', init, re.M)
     if not match:
         raise RuntimeError('Unable to find version string.')
     return match.group(1)
+
+
+requirements = []
+
+with open("requirements.txt") as f:
+    requirements.extend(f.read().splitlines())
 
 setup(
     name='superannotate_schemas',
@@ -20,6 +27,7 @@ setup(
     version=get_version(),
     package_dir={"": "src"},
     packages=find_packages(where="src"),
+    install_requires=requirements,
     description='SuperAnnotate JSON Schemas',
     author='Vaghinak Basentsyan',
     author_email='vaghinak@superannotate.con',
@@ -27,6 +35,6 @@ setup(
     license='MIT',
     description_file="README.md",
     entry_points={
-            'console_scripts': ['superannotate_schemas = superannotate_schemas.bin.app:main']
-        },
-      )
+        'console_scripts': ['superannotate_schemas = superannotate_schemas.bin.app:main']
+    },
+)
